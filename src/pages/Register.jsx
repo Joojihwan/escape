ba566cc5ceb567abf4acaf96fa400989950d513f
Joojihwan <wngustnf@naver.com>
2022-12-27 21:47:49 +0900

    Set layout of register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,8 +5,16 @@ import Footer from '../components/ui/Footer';
 function Register () {
     return (
         <div className="register">
-            <div className="register_button_area"></div>
-            <div className="register_content"></div>
+            <div className="register_button_area">
+                <button>등록</button>
+            </div>
+            <div className="register_content">
+                <div className="title"><label>title</label><input/></div>
+                <div className="category"><label>category</label><input/></div>
+                <div className="picture"><label>picture</label></div>
+                <div className="price"><label>price</label><input/></div>
+                <div className="flavor"><label>flavor</label></div>
+            </div>
             <Footer />
         </div>
     );
